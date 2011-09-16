fb7a45d2757ff9f42c22043546f15cf69e0aa861
박승현 <ppillip@gmail.com>
2011-09-16 00:36:41 -0700

    modify

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
-var net = require('net');
+
 var sys = require('sys');
+var net = require('net');
+var client = new net.Socket();
 
-var conCnt = 0;
-var LF = "\r\n";
+client.connect(2000,"localhost");
 
-var server = net.createServer(function (socket) {
+console.log("@@@@ start @@@@");
 
-    socket.on("connect",function(){
-        conCnt++;
-        socket.write("hello"+LF);
-        sys.print(LF+conCnt);
-    });
-
-    socket.on("data",function(data){
-
-       if(data=="nice"+LF){
-         socket.write("정답입니다"+LF);
-       }
-       else if(data=="exit"+LF){
-         socket.write("바이바이 꺼져라"+LF);
-         socket.end();
-       }
-       else {
-         socket.write(data);
-       }
-    })
-
+client.write("hi",'utf8',function(data){
+ console.log(data);
 });
 
-server.listen(1337, "127.0.0.1");
+
+
+client.end();
+
+console.log("@@@@ end @@@@");
