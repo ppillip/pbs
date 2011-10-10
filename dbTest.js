f452c9397489be8cfa6e7b16c55ed537d4c9749f
박승현 <ppillip@gmail.com>
2011-10-10 22:48:26 +0900

    디비 테스

--- dbTest.js
+++ dbTest.js
@@ -3,15 +3,18 @@ var server = new Mongolian("dbh56.mongolab.com:27567");
 
 var db = server.db("pcc");
 
-db.auth("ppillip", "dnflemf");
+db.auth("ppillip", "dnflemf",function(){
+    var lnc = db.collection("lnc");
 
-var lnc = db.collection("lnc");
+    lnc.find({}).sort({ lncID : 1 }).toArray(function(err,array){
+        array.forEach(function(dt){
+            console.log(dt.lncID);
+        });
+    })
+});
 
-lnc.find({}).sort({ lncID : 1 }).toArray(function(err,array){
-    array.forEach(function(dt){
-        console.log(dt.lncID);
-    });
-})
+
+
 
 
 
