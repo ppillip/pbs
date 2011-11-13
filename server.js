03d92d82cf3116b551eb80a766c5ae9d60f87486
박승현 <ppillip@gmail.com>
2011-11-13 16:03:10 +0900

    조금씩. 조금씩.

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ var dbServer = new Mongolian; //TODO config 로 설정
 var db = dbServer.db("pcc");
 var lnc = db.collection("lnc");
 
-var processTimeoutInterval = 2000;
+var processTimeoutInterval = 400;
 var slaveTimeout = 300; //슬레이브 기다려주는 시간 더 걸리면 짤없음 하드웨어 랑합의 봄
 var delay = 100;        //나머지 처리 시간을 감안해서 준다 (일종의 보정치)
 
 var Queue = [];
 
 //발사.
-console.log("1. Fire!");
+cl("1. Fire!");
 
 setDefaultQueue(function(){
     processQueue();
@@ -23,7 +23,7 @@ setDefaultQueue(function(){
 
 
 function setDefaultQueue(callback){
-    console.log("========================== setDefaultQueue ===========================");
+    cl("========================== setDefaultQueue ===========================");
     getLNCList(function(LNC){
 
         for(var i=0;i<LNC.length;i++){
@@ -54,13 +54,13 @@ function setDefaultQueue(callback){
                            var buf1 = new Buffer(data);
 
                            socket.write( buf1,function(){
-                               console.log("---------------------- 1. 보내고 ----------------------");
-                               console.log(buf1);
+                               cl("---------------------- 1. 보내고 ----------------------");
+                               cl(buf1);
                            });
 
                            socket.on("data",function(data){
-                               console.log(data);
-                               console.log("---------------------- 2. 받고 ----------------------");
+                               cl(data);
+                               cl("---------------------- 2. 받고 ----------------------");
                                callback2(data);
                                socket.end();
                                socket.destroy();
@@ -84,37 +84,49 @@ var x = null;
 //무한루프.
 function processQueue(){
 
-    console.log("2. processQueue");
+
 
     //큐에 있으면 일단 지운다.
     var _obj=null;
     _obj = Queue.pop();
     if(_obj){
+        cl("발사.");
         switch(_obj.procType){
             case "slaveScan" :
                 _obj.proc(function(data){
                     var buf = new Buffer(data);
-                    console.log("======================slaveScan=================lnc : "+buf[2]);
+                    cl("======================slaveScan=================lnc : "+buf[2]);
+
+                    //전광판 변경
+                    fncSignPanel(data, function() {});
+                    //전체 상태 업데이트 DB or Memory
+                    fncUpdateStatus(data, function() {});
+                    //모니터링 쪽으로 전송
+                    fncSendToPMS(data, function() {});
+
                 });
             break;
 
             case "webPush" :
-                _obj.proc(function(param){
-                    console.log(param.cmd);
+                _obj.proc(function(res,param){
+                    cl("======================webPush=================== : "+param.cmd + "[" + param.data + "]");
+                    res.writeHead(200, {'Content-Type': 'text/plain'});
+                    res.end("<b>"+param.cmd + "[" + param.data + "]");
+
                 })
             break;
 
             default :
-                console.log("Proc 형태를 알수가 없습니다.");
+                cl("Proc 형태를 알수가 없습니다.");
             break;
         }
 
        x = setTimeout(function(){processQueue();},_obj.timeoutInterval + 1);
-       //console.log(typeof(_obj.timeoutInterval));
+       //cl(typeof(_obj.timeoutInterval));
     }
     else
     {
-        console.log("큐가비었네요.");
+        cl("큐가비었네요.");
         setDefaultQueue(function(){
            x = setTimeout(function(){processQueue();},1000);
         });
@@ -122,8 +134,30 @@ function processQueue(){
 
 };
 
+/*
+복잡다단한전광판로직..
+ */
+function fncSignPanel(data, callback){
+  cl("fncSignPanel");
+};
+/*
+update db status
+ */
+function fncUpdateStatus(data, callback){
+    cl("fncUpdateStatus");
+};
+
+/*
+send to pms
+ */
+function fncSendToPMS(data, callback){
+    cl("fncSendToPMS");
+};
+
+
+
 function getLNCList(callback){
-    console.log("getLNCList()");
+    cl("getLNCList()");
 
     var lncList = [];
 
@@ -131,41 +165,38 @@ function getLNCList(callback){
         array.forEach(function(dt){
             lncList.unshift(dt.lncID);
         });
-        console.log(lncList.length + "개 준다");
+        cl(lncList.length + "개 준다");
         callback(lncList);
     })
 
 };
 
+/*
+        web pusher
+         //사용법 http://localhost:1337/?cmd=1234&data=hiall
+ */
 
-//사용법 http://localhost:1337/?cmd=1234&data=hiall
 var httpFromPMS = require('http');
 httpFromPMS.createServer(function (req, res) {
 
     var obj = require('url').parse(req.url, true);
 
-    //어케리턴하지?
+    cl("push to Queue ==========================================================> cmd="+obj.query.cmd + "&data=" + obj.query.data);
     Queue.unshift(
         {
             'procType':'webPush'
             ,'proc' : function(callback){
-                console.log(obj.query.cmd + "=====================" + obj.query.data);
-
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end("11111111111111111111111111");
-
-                callback(obj.query);
+                callback(res,obj.query);
             }
             ,'timeoutInterval' : 1000
         }
     );
 
-
-
-
-//  fncCmdFromPMS(obj.query,function(data){
-//      res.writeHead(200, {'Content-Type': 'text/plain'});
-//      res.end(data);
-//  });
 }).listen(1337, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+cl('Server running at http://127.0.0.1:1337/');
+
+
+
+function cl(str){
+    console.log("##"+(new Date())+"##"+str);
+}
